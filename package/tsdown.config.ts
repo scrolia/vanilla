6fbaa57b2af5741404367fef9cc7b9802920370d
Alpheus <26622568+alpheustangs@users.noreply.github.com>
2025-06-26 16:31:30 +0800

    Migrate to tsdown

---
+++ package/tsdown.config.ts
@@ -0,0 +1,35 @@
+import type { Options } from "tsdown";
+
+import { defineConfig } from "tsdown";
+
+const options: Options = {
+    entry: {
+        index: "./src/index.ts",
+    },
+    dts: false,
+    outDir: "./dist",
+    clean: true,
+    platform: "browser",
+    treeshake: true,
+    sourcemap: true,
+    minify: false,
+    shims: true,
+    unbundle: false,
+    inputOptions: {
+        experimental: {
+            attachDebugInfo: "none",
+        },
+    },
+};
+
+export default defineConfig([
+    {
+        ...options,
+        format: "esm",
+    },
+    {
+        ...options,
+        dts: true,
+        format: "cjs",
+    },
+]);
